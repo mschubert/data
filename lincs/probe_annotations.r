56d0b542ed336e0d3c255dd541f1ce2ce03ce2ca
mschubert <mschu.dev@gmail.com>
2017-01-12 18:50:03 +0000

    test probe annots w/ all biomart sets (not useful)

--- lincs/probe_annotations.r
+++ lincs/probe_annotations.r
@@ -1,14 +1,41 @@
 io = import('ebits/io')
+probes = import('./probes')
+mart = biomaRt::useMart(biomart="ensembl", dataset="hsapiens_gene_ensembl")
+
+affy_annots = c(
+    'affy_hg_u133_plus_2', # 20052 of 22268
+    "affy_hg_focus",
+    "affy_hg_u133_plus_2",
+    "affy_hg_u133a_2",
+    "affy_hg_u133a",
+    "affy_hg_u133b",
+    "affy_hg_u95av2",
+    "affy_hg_u95b",
+    "affy_hg_u95c",
+    "affy_hg_u95e",
+    "affy_hg_u95d",
+    "affy_hg_u95a",
+    "affy_hugenefl",
+#   "affy_hta_2_0",
+#    "affy_huex_1_0_st_v2",
+#    "affy_hugene_1_0_st_v1",
+    "affy_primeview",
+#    "affy_hugene_2_0_st_v1",
+    "affy_u133_x3p"
+)
+
+get_annot = function(probe_array) {
+    annot = biomaRt::getBM(attributes = c("hgnc_symbol", "hgnc_id", "entrezgene",
+        "ensembl_gene_id", "ensembl_transcript_id", probe_array),
+        filter = probe_array, values = probes$projected, mart=mart)
+    annot[annot==''] = NA
+    annot
+}
 
 probe_annotations = function() {
-    fname = module_file("cache", 'annot.RData', mustWork=TRUE) #FIXME: get path also when the file doesn't exist
+    fname = module_file("cache", 'annot.RData')
     if (!file.exists(fname)) {
-        mart = biomaRt::useMart(biomart="ensembl", dataset="hsapiens_gene_ensembl")
-        annot = biomaRt::getBM(attributes = c("hgnc_symbol", "hgnc_id", "entrezgene",
-            "ensembl_gene_id", "ensembl_transcript_id", "affy_hg_u133_plus_2"),
-            filter = "affy_hg_u133_plus_2", values = projected, mart=mart)
-        annot[annot==''] = NA
-
+        annot = get_annot('affy_hg_u133_plus_2')
         save(annot, file=fname)
         annot
     } else
