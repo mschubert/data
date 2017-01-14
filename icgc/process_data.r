90479a7e2954306897dabf9ab21afcd8c9004a98
mschubert <mschu.dev@gmail.com>
2017-01-14 12:38:27 +0000

    split processing funcs

--- icgc/process_data.r
+++ icgc/process_data.r
@@ -1,44 +1,46 @@
 util = import('./process_util')
 
-#' Function that processes a raw data download from ICGC to .RData/hdf5 files that can be used for analysis
+#' Process rna_seq data
 #'
-#' @param force  overwrite existing files instead of skipping
-process_raw_data = function(force=FALSE) {
-    tfun = function(x) mutate(x, tissue = .b$grep("^(\\w+)", project_code))
-
-    util$df("clinical.RData", "clinical\\.", transform=tfun, force=force)
-    util$df("clinicalsample.RData", "clinicalsample\\.", force=force)
+#' @param force  Overwrite existing files instead of skipping
+process_rna_seq = function(force=FALSE) {
     util$mat("expr_seq_raw.h5", '^exp_seq',
         raw_read_count ~ gene_id + icgc_sample_id, map.hgnc=TRUE, force=force)
-    util$mat("expr_seq_norm.h5", '^exp_seq',
-        normalized_read_count ~ gene_id + icgc_sample_id, map.hgnc=TRUE, force=force)
-    util$mat("protein.h5", '^protein_expression',
-        normalized_expression_level ~ antibody_id + icgc_sample_id, map.hgnc=FALSE, force=force)
 
-#    util$mat("cnv.h5", '^copy_number_somatic_mutation',
-#             segment_median ~ gene_affected + icgc_sample_id,
-#             fun.aggregate = mean, map.hgnc=T, force=force)
-
-#    mut_aggr = function(x) {
-#        if (length(x) == 0)
-#            return(0)
-#        if (!is.numeric(x)) {
-#            mtypes = getMutationTypes(bits=TRUE)
-#            x = mtypes$bit_code[mtypes$consequence_type %in% x]
-#        }
-#        Reduce(bitwOr, x)
-#    }
-    mut_aggr = function(x) {
-        any(x != 0)
-    }
-    util$mat('mutations.h5', '^simple_somatic',
-             consequence_type ~ gene_affected + icgc_sample_id,
-             fun.aggregate = mut_aggr, force=force, map.hgnc=TRUE)
-
+#    util$mat("expr_seq_norm.h5", '^exp_seq',
+#        normalized_read_count ~ gene_id + icgc_sample_id, map.hgnc=TRUE, force=force)
     voomfile = file.path(icgc_data_dir, "expr_seq_voom.h5")
     if (identical(force, TRUE) || !file.exists(voomfile)) {
         expr = getRNASeq(raw.counts=TRUE) %>% na.omit()
         expr = limma::voom(expr)$E
         h5store::h5save(t(expr), file=voomfile)
     }
+
+}
+
+process_clinical = function(force=FALSE) {
+    tfun = function(x) mutate(x, tissue = .b$grep("^(\\w+)", project_code))
+    util$df("clinical.RData", "clinical\\.", transform=tfun, force=force)
+    util$df("clinicalsample.RData", "clinicalsample\\.", force=force)
+}
+
+process_mutations = function(force=FALSE) {
+    mut_aggr = function(x) {
+        any(x != 0)
+    }
+    util$mat('mutations.h5', '^simple_somatic',
+             consequence_type ~ gene_affected + icgc_sample_id,
+             fun.aggregate = mut_aggr, force=force, map.hgnc=TRUE)
+}
+
+process_cnv = function(force=FALSE) {
+    util$mat("cnv.h5", '^copy_number_somatic_mutation',
+             segment_median ~ gene_affected + icgc_sample_id,
+             fun.aggregate = mean, map.hgnc=T, force=force)
+}
+
+process_rppa = function(force=FALSE) {
+    util$mat("protein.h5", '^protein_expression',
+        normalized_expression_level ~ antibody_id + icgc_sample_id,
+        map.hgnc=FALSE, force=force)
 }
