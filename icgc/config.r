b07990f00e699c7eed66324548d9e0562e44511e
mschubert <mschu.dev@gmail.com>
2017-01-14 23:47:46 +0000

    make sure file paths work

--- icgc/config.r
+++ icgc/config.r
@@ -1,3 +1,6 @@
 version = "release_23"
-raw_data = module_file('./data', version)
-cached_data = module_file('./cache', version)
+raw_data = module_file('./data', version, mustWork=TRUE)
+cached_data = file.path(module_file('./cache'), version)
+
+if (!file.exists(cached_data))
+    dir.create(cached_data)
