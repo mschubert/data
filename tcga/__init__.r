859a2ed19275ddb08569c7f1ea81159869f6f9c6
mschubert <mschu.dev@gmail.com>
2017-01-17 16:24:36 +0000

    update make for split-get

--- tcga/__init__.r
+++ tcga/__init__.r
@@ -1,6 +1,10 @@
 export_submodule('./barcode')
-export_submodule('./get')
 export_submodule('./map_id')
 export_submodule('./filter')
+clinical = import('./clinical')$clinical
+cna = import('./cna')$cna
+mutations = import('./mutations')$mutations
+rna_seq = import('./rna_seq')$rna_seq
+rppa = import('./rppa')$rppa
 bem = import('./bem')$bem
 intersect = import('./intersect')$intersect
