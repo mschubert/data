67065d39dec6b4d4568b4f50a993dd9124b40b4e
mschubert <mschu.dev@gmail.com>
2017-01-25 15:46:54 +0000

    filter by TCGA

--- icgc/studies.r
+++ icgc/studies.r
@@ -2,8 +2,16 @@ config = import('./config')
 
 #' Return the index of all specimen
 #'
+#' @param tcga  Only (TRUE) or no (FALSE) TCGA cohorts
 #' @return  A data.frame of details of all specimen
-studies= function() {
+studies= function(tcga=NULL) {
     pdir = file.path(config$raw_data, "Projects")
-    list.files(pdir, include.dirs=TRUE)
+    studies = grep("[A-Z]+-[A-Z]+", list.files(pdir, include.dirs=TRUE), value=TRUE)
+
+    if (is.null(tcga))
+        studies
+    else if (tcga)
+        studies[grepl("-US$", studies)]
+    else
+        studies[!grepl("-US$", studies)]
 }
