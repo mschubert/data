41c814bf2179563486140ecb106300b92c17b70c
mschubert <mschu.dev@gmail.com>
2017-01-25 13:26:43 +0000

    export rna_seq2_vst

--- tcga/firehose/__init__.r
+++ tcga/firehose/__init__.r
@@ -1,6 +1,7 @@
 clinical = import('./clinical')$clinical
 rna_seq_voom = import('./rna_seq_voom')$rna_seq_voom
 rna_seq2_voom = import('./rna_seq2_voom')$rna_seq2_voom
+rna_seq2_vst = import('./rna_seq2_vst')$rna_seq2_vst
 mutations = import('./mutations')$mutations
 rppa = import('./rppa')$rppa
 cna = import('./cna')$cna
