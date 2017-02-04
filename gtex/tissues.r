3c38c517c402e0aa4d876fa1c4f1926687158830
mschubert <mschu.dev@gmail.com>
2017-02-04 21:32:50 +0000

    omit NAs for tissue by default

--- gtex/tissues.r
+++ gtex/tissues.r
@@ -1,17 +1,18 @@
 .samples = import('./samples')$samples()
+.omit = import('ebits/base/omit')
 
 #' Return tissue identifiers with sample IDs as names
 #'
 #' @param tissue  A character vector of which tissues to return
 #' @param detail  Use more detailed tissue descriptors
-tissues = function(tissue=NULL, detail=FALSE) {
+tissues = function(tissue=NULL, detail=FALSE, na_rm=TRUE) {
     field = "SMTS"
     if (detail)
         field = paste0(field, "D")
 
     re = setNames(.samples[[field]], .samples$SAMPID)
     if (is.null(tissue))
-        re
+        .omit$na(re, omit=na_rm)
     else
-        re[re %in% tissue]
+        .ommit$na(re[re %in% tissue], omit=na_rm)
 }
