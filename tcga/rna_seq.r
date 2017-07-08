da56b0e52aec0edb8b44212a2cd300325d5b299d
mschubert <mschu.dev@gmail.com>
2017-07-08 22:05:51 +0100

    allow gene selection

--- tcga/rna_seq.r
+++ tcga/rna_seq.r
@@ -8,18 +8,25 @@
 #'
 #' @param tissue   The tissue(s) to get expression for
 #' @param id_type  Where to cut the barcode, either "patient", "specimen", or "full"
+#' @param genes    Genes to retrieve (default: all)
 #' @return         A matrix with HGNC symbols x TCGA samples
-rna_seq = function(tissue, id_type="specimen", ...) {
+rna_seq = function(tissue, id_type="specimen", genes=TRUE, ...) {
     library(methods) # required; otherwise h5 error
     file = h5::h5file(module_file("cache", "rna_seq2_vst.gctx"), mode="r")
 
     barcodes = file["/0/META/COL/id"][]
     studies = .bc$barcode2study(barcodes)
-    keep = studies %in% tissue
+    sample_idx = which(studies %in% tissue)
 
-    data = file["/0/DATA/0/matrix"][which(keep),]
-    rownames(data) = barcodes[keep]
-    colnames(data) = file["/0/META/ROW/id"][]
+    all_genes = file["/0/META/ROW/id"][]
+    if (identical(genes, TRUE))
+        gene_idx = TRUE
+    else
+        gene_idx = match(genes, all_genes)
+
+    data = file["/0/DATA/0/matrix"][sample_idx, gene_idx]
+    rownames(data) = barcodes[sample_idx]
+    colnames(data) = all_genes[gene_idx]
 
     h5::h5close(file)
     .map_id(t(data), id_type=id_type, ...)
