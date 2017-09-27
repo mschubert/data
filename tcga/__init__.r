4e35eb0f899266418f3b02c80ede4207b97242f4
mschubert <mschu.dev@gmail.com>
2017-09-27 09:55:36 +0000

    export as well

--- tcga/__init__.r
+++ tcga/__init__.r
@@ -4,6 +4,7 @@ export_submodule('./filter')
 clinical = import('./clinical')$clinical
 cohorts = import('./cohorts')$cohorts
 cna = import('./cna')$cna
+cna_absolute = import('./cna_absolute')$cna_absolute
 mutations = import('./mutations')$mutations
 rna_seq = import('./rna_seq')$rna_seq
 rna_seq_genes = import('./rna_seq_genes')$rna_seq_genes
