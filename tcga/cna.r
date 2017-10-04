935de45b176ec60bfc11a3b7a5d8d311230b3538
mschubert <mschu.dev@gmail.com>
2017-10-04 15:19:38 +0000

    update loading to hdf5

--- tcga/cna.r
+++ tcga/cna.r
@@ -6,20 +6,34 @@
 
 #' Get a data.frame listing all GISTIC scores for CNAs
 #'
+#' @param tissue   The tissue(s) to get expression for
 #' @param id_type  Where to cut the barcode, either "patient", "specimen", or "full"
 #' @return         A data.frame with data for all the simple mutations
-cna_gistic = function(id_type="specimen", ...) {
-    .load("cache", "cna.RData") %>%
-        .map_id(id_type=id_type, along="barcode", ...)
-}
+cna_gistic = function(tissue, id_type="specimen", thresh=FALSE, ...) {
+    if (thresh)
+        fname = "cna_gistic_thresholded.gctx"
+    else
+        fname = "cna_gistic.gctx"
 
-#' Get a data.frame listing all GISTIC scores for CNAs
-#'
-#' @param id_type  Where to cut the barcode, either "patient", "specimen", or "full"
-#' @return         A data.frame with data for all the simple mutations
-cna_thresholded = function(id_type="specimen", ...) {
-    .load("cache", "cna_thresholded.RData") %>%
-        .map_id(id_type=id_type, along="barcode", ...)
+    library(methods) # required; otherwise h5 error
+    file = h5::h5file(module_file("cache", fname), mode="r")
+
+    barcodes = file["/0/META/COL/id"][]
+    studies = .bc$barcode2study(barcodes)
+    sample_idx = which(studies %in% tissue)
+
+    all_genes = file["/0/META/ROW/id"][]
+    if (identical(genes, TRUE))
+        gene_idx = seq_along(all_genes)
+    else
+        gene_idx = match(genes, all_genes)
+
+    data = file["/0/DATA/0/matrix"][sample_idx, gene_idx]
+    rownames(data) = barcodes[sample_idx]
+    colnames(data) = all_genes[gene_idx]
+
+    h5::h5close(file)
+    .map_id(t(data), id_type=id_type, ...)
 }
 
 #' Get ABSOLUTE copy numbers from synapse
