6b482824e62a89ed950c90a06c4b5708468c2e33
mschubert <mschu.dev@gmail.com>
2019-09-17 20:40:11 +0200

    use mirNA data from the GDC

--- tcga/mirna_seq.r
+++ tcga/mirna_seq.r
@@ -3,28 +3,29 @@
 .filter = import('./filter')$filter
 .map_id = import('./map_id')$map_id
 
-.load = function(...) .io$load(module_file(..., mustWork=TRUE))
-
 #' Get a matrix with miRNA expression (variance-stabilization transformed)
 #'
 #' @param tissue   Character vector of studies to include
 #' @param id_type  Where to cut the barcode, either "patient", "specimen", or "full"
-#' @param ...      Passed to map_id()
+#' @param trans    "raw" or "rpm"
+#' @param ...      Paramters passed to tcga$filter
 #' @return         A matrix [genes x samples]
-mirna_seq = function(tissue, id_type="specimen", ...) {
-    .load("cache", "mir_seq.RData") %>%
-        .filter(tissue=tissue) %>%
-        .map_id(id_type=id_type, ...)
-}
+mirna_seq = function(tissue, id_type="specimen", trans="raw", ...) {
+    fpath = module_file("TCGAbiolinks-downloader/mirna_seq", mustWork=TRUE)
+    expr = .io$load(file.path(fpath, paste0("TCGA-", tissue, ".RData")))
 
-#' Get a matrix with mature miRNA expression (log2 RPM)
-#'
-#' @param tissue   Character vector of studies to include
-#' @param id_type  Where to cut the barcode, either "patient", "specimen", or "full"
-#' @param ...      Passed to map_id()
-#' @return         A matrix [genes x samples]
-mirna_seq_mature = function(tissue, id_type="specimen", ...) {
-    .load("cache", "mir_seq_mature.RData") %>%
-        .filter(tissue=tissue) %>%
-        .map_id(id_type=id_type, ...)
+    if (trans == "raw")
+        keep = "^read_count_"
+    else if (trans == "rpm")
+        keep = "^reads_per_million_"
+    else
+        stop("Transformation ", sQuote(trans), " mot recognized")
+
+    dset = data.matrix(expr[,grepl(keep, colnames(expr))])
+    colnames(dset) = sub(keep, "", colnames(dset))
+    rownames(dset) = expr$miRNA_ID
+
+    dset %>%
+        .filter(...) %>%
+        .map_id(id_type=id_type)
 }
