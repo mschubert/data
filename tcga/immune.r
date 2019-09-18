bf6731ee58b687edfaa202aeef22b123afe283f3
mschubert <mschu.dev@gmail.com>
2019-09-18 19:26:33 -0400

    add Thorsson et al. immune atlas

---
+++ tcga/immune.r
@@ -0,0 +1,9 @@
+`%>%` = magrittr::`%>%`
+
+#' Get Pan-cancer immune measures from Thorsson et al.
+immune = function() {
+    fpath = module_file("data", mustWork=TRUE)
+    readxl::read_xlsx(file.path(fpath, "1-s2.0-S1074761318301213-mmc2.xlsx")) %>%
+        dplyr::rename(barcode = `TCGA Participant Barcode`,
+                      cohort = `TCGA Study`)
+}
