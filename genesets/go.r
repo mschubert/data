03ee1a2ee28de45a987f8f362508d8ca60b9260c
mschubertv <mschu.dev@gmail.com>
2021-02-26 10:39:54 +0000

    allow MF, CC ontologies

--- genesets/go.r
+++ genesets/go.r
@@ -13,7 +13,6 @@ import_package('dplyr', attach=TRUE)
 #' @param as_list   Whether to return a list of character vectors
 go = function(dset="hsapiens_gene_ensembl", genes="hgnc_symbol", valid=NULL,
               min_n=4, ontology="BP", leaf_depth=Inf, as_list=FALSE) {
-    stopifnot(ontology == "BP") #todo: not implemented
     fname = file.path(module_file("cache", mustWork=TRUE),
                       paste0(paste("go", dset, genes, sep="-"), ".rds"))
     if (file.exists(fname)) {
@@ -23,7 +22,7 @@ go = function(dset="hsapiens_gene_ensembl", genes="hgnc_symbol", valid=NULL,
         mart = biomaRt::useMart(biomart="ensembl", dataset=dset)
         mapGO = biomaRt::getBM(attributes=c(genes, "go_id"), mart=mart)
         sets = mapGO %>% dplyr::filter(go_id != "")
-        sets = sets[sets[[genes]] != "",]
+        sets = tibble::as_tibble(sets[sets[[genes]] != "",])
         saveRDS(sets, file=fname)
     }
 
@@ -31,14 +30,14 @@ go = function(dset="hsapiens_gene_ensembl", genes="hgnc_symbol", valid=NULL,
         valid = unique(sets[[genes]])
     genes_per_set = sapply(unstack(sets), length)
 
-    BP = AnnotationDbi::toTable(GO.db::GOBPPARENTS)
+    obj = get(paste0("GO", ontology, "PARENTS"), asNamespace("GO.db"))
+    tree = AnnotationDbi::toTable(obj)
 
     terms = tibble::as_tibble(AnnotationDbi::toTable(GO.db::GOTERM)[,2:5])
     terms = terms[!duplicated(terms$go_id),]
-    terms = terms[terms$go_id %in% unlist(BP[1:2]),]
 
     valid_sets = intersect(terms$go_id, names(genes_per_set)[genes_per_set >= min_n])
-    g = igraph::graph.data.frame(BP, vertices=terms)
+    g = igraph::graph.data.frame(tree, vertices=terms)
     gs = igraph::induced_subgraph(g, valid_sets)
 
     if (is.finite(leaf_depth)) {
@@ -51,7 +50,7 @@ go = function(dset="hsapiens_gene_ensembl", genes="hgnc_symbol", valid=NULL,
         terms = terms[terms$go_id %in% return_sets,]
     }
 
-    res = dplyr::inner_join(terms, sets)
+    res = dplyr::inner_join(terms, sets, by="go_id")
     if (as_list) {
         res$both = paste(res$go_id, res$Term)
         unstack(res[c(genes, "both")])
