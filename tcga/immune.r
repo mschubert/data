2347eef681fc71aa03be21f54fc0d4f21c17d150
mschubertv <mschu.dev@gmail.com>
2021-05-31 18:35:05 +0000

    map NAs

--- tcga/immune.r
+++ tcga/immune.r
@@ -3,7 +3,7 @@
 #' Get Pan-cancer immune measures from Thorsson et al.
 immune = function() {
     fpath = module_file("data", mustWork=TRUE)
-    readxl::read_xlsx(file.path(fpath, "1-s2.0-S1074761318301213-mmc2.xlsx")) %>%
+    readxl::read_xlsx(file.path(fpath, "1-s2.0-S1074761318301213-mmc2.xlsx"), na="NA") %>%
         dplyr::rename(barcode = `TCGA Participant Barcode`,
                       cohort = `TCGA Study`)
 }
