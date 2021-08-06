f053f8ae2a3b4551bcd757290d4acb69641a5ebf
mschubertv <mschu.dev@gmail.com>
2021-08-06 10:16:24 +0000

    add ABSOLUTE ploidy, aneup

--- tcga/__init__.r
+++ tcga/__init__.r
@@ -6,6 +6,7 @@ export_submodule('./cna_old')
 export_submodule('./mirna_seq')
 export_submodule('./meth_cpg')
 export_submodule('./purity')
+export_submodule('./aneuploidy')
 clinical = import('./clinical')$clinical
 cohorts = import('./cohorts')$cohorts
 mutations = import('./mutations')$mutations
@@ -16,6 +17,5 @@ rna_seq_genes = import('./rna_seq_genes')$rna_seq_genes
 rppa = import('./rppa')$rppa
 bem = import('./bem')$bem
 intersect = import('./intersect')$intersect
-aneuploidy = import('./aneuploidy')$aneuploidy
 meth = import('./methylation')$methylation
 immune = import('./immune')$immune
--- tcga/aneuploidy.r
+++ tcga/aneuploidy.r
@@ -2,6 +2,7 @@
 sys = import('sys')
 seq = import('seq')
 .cna_segments = import('./cna')$cna_segments
+.cna_absolute = import('./cna_old')$cna_absolute
 
 #' Provide AneuFinder-like aneuploidy score
 #'
@@ -10,8 +11,16 @@ seq = import('seq')
 #' @param ...     Arguments passed to `seq$aneuploidy`
 #' @return  ...
 aneuploidy = function(cohort=NULL, assembly="GRCh38", per_chromosome=FALSE, ...) {
-    cna = .cna_segments(cohort) %>%
+    .cna_segments(cohort) %>%
         dplyr::mutate(width = abs(End - Start)) %>%
         seq$aneuploidy(assembly=assembly, per_chromosome=per_chromosome,
                        sample="Sample", seqnames="Chromosome")
+}
+
+aneuploidy_absolute = function(cohort=NULL) {
+    .cna_absolute(cohort) %>%
+        group_by(Sample) %>%
+            summarize(abs_ploidy = weighted.mean(Modal_HSCN_1 + Modal_HSCN_2, Length),
+                      abs_aneup = weighted.mean(abs(Modal_HSCN_1 + Modal_HSCN_2 - 2), Length)) %>%
+        ungroup()
 }
