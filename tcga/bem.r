d82f76904fb6e132fb648ef2fe50e2c04cd53b14
mschubertv <mschu.dev@gmail.com>
2021-10-15 12:39:52 +0000

    add links, comment

--- tcga/bem.r
+++ tcga/bem.r
@@ -2,6 +2,13 @@ b = import('ebits/base')
 io = import('ebits/io')
 
 bem = function() {
+#    bem = paste0("https://www.cancerrxgene.org/gdsc1000/GDSC1000_WebResources/Data/", c(
+#        mut = "BEMs/PrimaryTumours/PrimTum_CG_BEMs.zip", # link broken
+#        cna = "suppData/TableS2F.xlsx",
+#        meth = "BEMs/PrimaryTumours/PrimTum_METH_BEMs.zip"
+#    ))
+
+    # only present when all data avail -> e.g. only 400 BRCA samples
     io$load(module_file("cache", "BEM.RData"))
 }
 
