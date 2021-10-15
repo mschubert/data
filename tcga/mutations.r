0d9c50262c3784e9c436fb4d64a25c2c83f857ff
mschubertv <mschu.dev@gmail.com>
2021-10-15 13:05:42 +0000

    use GDC mutect2 calls

--- tcga/mutations.r
+++ tcga/mutations.r
@@ -1,19 +1,14 @@
 `%>%` = magrittr::`%>%`
-.io = import('ebits/io')
 .map_id = import('./map_id')$map_id
-
-.load = function(...) .io$load(module_file(..., mustWork=TRUE))
 
 #' Get a data.frame listing all mutations and types
 #'
 #' @param id_type  Where to cut the barcode, either "patient", "specimen", or "full"
 #' @return         A data.frame with data for all the simple mutations
-mutations = function(id_type="specimen", ...) {
-    # 19k/22k in READ and all 20k OV have no portion in barcode, assuming 'A'
-    .load("cache", "mutations.RData") %>%
-        dplyr::mutate(Tumor_Sample_Barcode =
-                      ifelse(nchar(Tumor_Sample_Barcode) == 15,
-                             paste0(Tumor_Sample_Barcode, "A"),
-                             Tumor_Sample_Barcode)) %>%
-        .map_id(id_type=id_type, along="Tumor_Sample_Barcode", ...)
+mutations = function(cohort, id_type="specimen", ...) {
+    fdir = module_file("TCGAbiolinks-downloader", "snv_mutect2")
+    fname = file.path(fdir, sprintf("TCGA-%s.rds", cohort))
+    tibble::as_tibble(readRDS(fname)) %>%
+        dplyr::mutate(Sample = .map_id(Tumor_Sample_Barcode, id_type=id_type)) %>%
+        select(Sample, everything())
 }
